6155b866abe934613b97dfa4c01873ae9dc9ac09
kimminju99 <mindori1019@gmail.com>
2022-08-02 22:48:07 +0900

    Feature: activity_signin.xml 예시 생성 및 font 생성

---
+++ app/src/main/java/com/bf/helpergomdori/ui/signIn/SignInActivity.kt
@@ -0,0 +1,17 @@
+package com.bf.helpergomdori.ui.signIn
+
+import android.os.Bundle
+import com.bf.helpergomdori.R
+import com.bf.helpergomdori.base.BaseActivity
+import com.bf.helpergomdori.databinding.ActivitySigninBinding
+
+class SignInActivity : BaseActivity() {
+    private val binding by bindingRes<ActivitySigninBinding>(R.layout.activity_signin)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding.apply {
+
+        }
+    }
+}
