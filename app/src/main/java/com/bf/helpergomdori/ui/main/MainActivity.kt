28c5f85f71d3848c91ac304462518a8f03cc276d
kimminju99 <mindori1019@gmail.com>
2022-08-02 22:00:09 +0900

    feature: 앱 구동을 위해 mainActivity 생성

---
+++ app/src/main/java/com/bf/helpergomdori/ui/main/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.bf.helpergomdori.ui.main
+
+import android.os.Bundle
+import com.bf.helpergomdori.R
+import com.bf.helpergomdori.base.BaseActivity
+import com.bf.helpergomdori.databinding.ActivityMainBinding
+
+class MainActivity : BaseActivity() {
+    private val binding by bindingRes<ActivityMainBinding>(R.layout.activity_main)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding.apply {  }
+    }
+}
