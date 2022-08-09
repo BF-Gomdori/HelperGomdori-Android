b15d23a9dc937050db219db91860bd418912dade
kimminju99 <mindori1019@gmail.com>
2022-08-09 21:28:02 +0900

    Feature: 가입하기 큰 틀 완성 (Jetpack Navigation 사용)

--- app/src/main/java/com/bf/helpergomdori/ui/signIn/SignInGreetingFragment.kt
+++ app/src/main/java/com/bf/helpergomdori/ui/signIn/SignInGreetingFragment.kt
@@ -1,17 +1,25 @@
 package com.bf.helpergomdori.ui.signIn
 
+import androidx.navigation.findNavController
 import com.bf.helpergomdori.R
 import com.bf.helpergomdori.base.BaseFragment
 import com.bf.helpergomdori.databinding.FragmentSigninGreetingBinding
 
 class SignInGreetingFragment : BaseFragment<FragmentSigninGreetingBinding>(R.layout.fragment_signin_greeting) {
     override fun createView(binding: FragmentSigninGreetingBinding) {
-        binding.apply {
-
-        }
+        setupClicks()
     }
 
     override fun createFragment() {
 
+    }
+
+    private fun setupClicks() {
+        binding!!.apply {
+            btnNaverLogin.setOnClickListener {
+                val navController = it.findNavController()
+                navController.navigate(R.id.signInHelpingFragment)
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/bf/helpergomdori/ui/signIn/SignInHelpeeFragment.kt
@@ -0,0 +1,17 @@
+package com.bf.helpergomdori.ui.signIn
+
+import com.bf.helpergomdori.R
+import com.bf.helpergomdori.base.BaseFragment
+import com.bf.helpergomdori.databinding.FragmentSigninHelpeeBinding
+
+class SignInHelpeeFragment: BaseFragment<FragmentSigninHelpeeBinding>(R.layout.fragment_signin_helpee) {
+    override fun createView(binding: FragmentSigninHelpeeBinding) {
+        binding.apply {
+
+        }
+    }
+
+    override fun createFragment() {
+
+    }
+}
