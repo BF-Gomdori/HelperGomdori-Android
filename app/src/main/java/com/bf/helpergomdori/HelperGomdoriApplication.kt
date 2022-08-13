43f6d3d8acdf6688e8ec997c646edf6ea3b3a61e
kimminju99 <mindori1019@gmail.com>
2022-08-13 22:12:34 +0900

    Feature: naver maps 연동

--- app/src/main/java/com/bf/helpergomdori/HelperGomdoriApplication.kt
+++ app/src/main/java/com/bf/helpergomdori/HelperGomdoriApplication.kt
@@ -3,6 +3,9 @@ package com.bf.helpergomdori
 import android.app.Application
 import com.bf.helpergomdori.utils.NATIVE_APP_KEY
 import com.kakao.sdk.common.KakaoSdk
+import com.naver.maps.map.NaverMapSdk
+import com.naver.maps.map.NaverMapSdk.NaverCloudPlatformClient
+
 
 class HelperGomdoriApplication: Application() {
     override fun onCreate() {
--- app/src/main/java/com/bf/helpergomdori/ui/main/MainActivity.kt
+++ app/src/main/java/com/bf/helpergomdori/ui/main/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.bf.helpergomdori.ui.main
 
-import android.os.Bundle
+
 import com.bf.helpergomdori.R
 import com.bf.helpergomdori.base.BaseActivity
 import com.bf.helpergomdori.databinding.ActivityMainBinding
+import com.naver.maps.map.MapFragment
 
-class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main){
+
+    private lateinit var mapFragment: MapFragment
+
     override fun createActivity() {
-        binding.apply {  }
+        binding.apply {
+
+        }
+
+        // 지도 객체 생성
+        mapFragment = supportFragmentManager.findFragmentById(R.id.map_fragment) as MapFragment
+
     }
+
 }
