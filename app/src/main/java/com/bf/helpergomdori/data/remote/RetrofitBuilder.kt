e48024252f004836c26e3d7c3c08ba1e45832e7c
kimminju99 <mindori1019@gmail.com>
2022-08-24 15:21:12 +0900

    Feature: okhttpClient 만들어서 logging(모니터링) 가능하게 하고 connection timeout 설정

--- app/src/main/java/com/bf/helpergomdori/data/remote/RetrofitBuilder.kt
+++ app/src/main/java/com/bf/helpergomdori/data/remote/RetrofitBuilder.kt
@@ -8,13 +8,14 @@ import retrofit2.converter.gson.GsonConverterFactory
 import javax.xml.datatype.DatatypeConstants.SECONDS
 
 object RetrofitBuilder {
+    //private const val BASE_URL = "https://reqres.in/api/" <- getUser
     private const val BASE_URL = "http://dbb5-219-255-158-172.ngrok.io"
 
     private val httpLoggingInterceptor = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BODY
     }
 
-    private val okHttpBuilder = OkHttpClient.Builder()
+    private val okHttpClient = OkHttpClient.Builder()
         .addNetworkInterceptor(httpLoggingInterceptor)
         .connectTimeout(100, TimeUnit.SECONDS)
         .readTimeout(100, TimeUnit.SECONDS)
@@ -24,6 +25,7 @@ object RetrofitBuilder {
     private fun getRetrofit() = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
+        .client(okHttpClient)
         .build()
 
     val apiService: ApiService = getRetrofit().create(ApiService::class.java)
