462cceaad8a9d21228a0613f67fbd81c405f929d
kimminju99 <mindori1019@gmail.com>
2022-08-26 14:23:24 +0900

    Feature: network 관련 코드에 CoroutineDispatcher 넣는 작업

--- app/src/main/java/com/bf/helpergomdori/data/repository/RemoteRepository.kt
+++ app/src/main/java/com/bf/helpergomdori/data/repository/RemoteRepository.kt
@@ -4,13 +4,15 @@ package com.bf.helpergomdori.data.repository
 import com.bf.helpergomdori.data.remote.RemoteDataSourceImpl
 import com.bf.helpergomdori.model.Data
 import com.bf.helpergomdori.model.body.PostUser
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class RemoteRepository @Inject constructor(
-    private val remoteDataSource: RemoteDataSourceImpl
-    //, private val externalScope: CoroutineScope
+    private val remoteDataSource: RemoteDataSourceImpl,
+    //private val externalScope: CoroutineScope
 ) {
-    suspend fun getData(): Data = remoteDataSource.getData()
+    suspend fun getData(): Flow<Data> = remoteDataSource.getData()
 
     suspend fun postUserInfo(postUser: PostUser) = remoteDataSource.postMember(postUser)
 }
