90ea2b4c082cc19b30b580ce643e0591a99240f6
kimminju99 <mindori1019@gmail.com>
2022-08-31 04:38:42 +0900

    feature/structure: requestHelp Activity 1개, Fragment 3개, navigation_graph 1개 생성

---
+++ app/src/main/java/com/bf/helpergomdori/ui/requestHelp/RequestHelpLocationFragment.kt
@@ -0,0 +1,16 @@
+package com.bf.helpergomdori.ui.requestHelp
+
+import com.bf.helpergomdori.R
+import com.bf.helpergomdori.base.BaseFragment
+import com.bf.helpergomdori.databinding.FragmentRequestHelpLocationBinding
+import com.bf.helpergomdori.databinding.FragmentRequestHelpWriteBinding
+
+class RequestHelpLocationFragment: BaseFragment<FragmentRequestHelpLocationBinding>(R.layout.fragment_request_help_location) {
+    override fun createView(binding: FragmentRequestHelpLocationBinding) {
+
+    }
+
+    override fun createFragment() {
+    }
+
+}
---
+++ app/src/main/java/com/bf/helpergomdori/ui/requestHelp/RequestHelpWriteFragment.kt
@@ -0,0 +1,13 @@
+package com.bf.helpergomdori.ui.requestHelp
+
+import com.bf.helpergomdori.R
+import com.bf.helpergomdori.base.BaseFragment
+import com.bf.helpergomdori.databinding.FragmentRequestHelpWriteBinding
+
+class RequestHelpWriteFragment: BaseFragment<FragmentRequestHelpWriteBinding>(R.layout.fragment_request_help_write) {
+    override fun createView(binding: FragmentRequestHelpWriteBinding) {
+    }
+
+    override fun createFragment() {
+    }
+}
