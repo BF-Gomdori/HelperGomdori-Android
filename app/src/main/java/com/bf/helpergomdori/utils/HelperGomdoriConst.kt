d6dee5d48bd2a52b61c58edb258772af399f9d4e
kimminju99 <mindori1019@gmail.com>
2022-08-31 15:21:57 +0900

    feature/main_response: 길찾기 연동

--- app/src/main/java/com/bf/helpergomdori/utils/HelperGomdoriConst.kt
+++ app/src/main/java/com/bf/helpergomdori/utils/HelperGomdoriConst.kt
@@ -26,7 +26,14 @@ const val LOCATION_PERMISSION_REQUEST_CODE = 1000
  * MainActivity
  */
 const val CAMERA_ZOOM_DENSITY = 15.5
-
+const val NAVER_MAP_SCHEME_BASE_URL = "nmap://route/walk?" // 도보 길찾기
+const val SLAT_PARAM = "slat"
+const val SLNG_PRARM = "slng"
+const val SNAME_PARAM = "sname"
+const val DLAT_PARAM = "dlat"
+const val DLNG_PARRAM = "dlng"
+const val DNAME_PARAM = "dname"
+const val APP_NAME_PARAM = "appname"
 
 /**
  * API
