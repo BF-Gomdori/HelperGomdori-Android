7750b822a085318ece43a44ea98bab53ef8a9832
snoopy <chococho99@gmail.com>
2022-09-01 15:14:09 +0900

    pull develop

--- app/src/main/java/com/bf/helpergomdori/ui/request/RequestMatchingFragment.kt
+++ app/src/main/java/com/bf/helpergomdori/ui/request/RequestMatchingFragment.kt
@@ -6,6 +6,9 @@ import com.bf.helpergomdori.databinding.FragmentRequestMatchingBinding
 
 class RequestMatchingFragment: BaseFragment<FragmentRequestMatchingBinding>(R.layout.fragment_request_matching) {
     override fun createView(binding: FragmentRequestMatchingBinding) {
+        binding.btnWeMet.setOnClickListener {
+            navController?.navigate(R.id.action_requestMatchingFragment_to_requestFinishedFragment)
+        }
     }
 
     override fun createFragment() {
