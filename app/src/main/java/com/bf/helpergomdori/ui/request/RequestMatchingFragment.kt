d9e921c55ab90f65d5c08a217321e14b47b82bfb
kimminju99 <mindori1019@gmail.com>
2022-09-04 13:31:28 +0900

    Feature/request: 만났어요 화면 ping 제대로 찍히게 기능 구현 완료

--- app/src/main/java/com/bf/helpergomdori/ui/request/RequestMatchingFragment.kt
+++ app/src/main/java/com/bf/helpergomdori/ui/request/RequestMatchingFragment.kt
@@ -1,11 +1,15 @@
 package com.bf.helpergomdori.ui.request
 
+import android.util.Log
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
 import com.bf.helpergomdori.R
 import com.bf.helpergomdori.base.BaseFragment
 import com.bf.helpergomdori.databinding.FragmentRequestMatchingBinding
+import com.bf.helpergomdori.model.websocket.Location
 import com.bf.helpergomdori.utils.CAMERA_ZOOM_DENSITY_MORE
 import com.bf.helpergomdori.utils.DensityUtil
+import com.bf.helpergomdori.utils.REQUEST_TAG
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.*
 import com.naver.maps.map.overlay.Marker
@@ -29,9 +33,9 @@ class RequestMatchingFragment :
     }
 
     private fun initView() {
-        mapFragment = requireActivity().supportFragmentManager.findFragmentById(R.id.map_fragment) as MapFragment?
+        mapFragment = requireActivity().supportFragmentManager.findFragmentById(R.id.map_matching_fragment) as MapFragment?
             ?: MapFragment.newInstance().also {
-                requireActivity().supportFragmentManager.beginTransaction().add(R.id.map_fragment, it).commit()
+                requireActivity().supportFragmentManager.beginTransaction().add(R.id.map_matching_fragment, it).commit()
             }
         mapFragment.getMapAsync(this)
     }
@@ -42,8 +46,21 @@ class RequestMatchingFragment :
         }
     }
 
+    private fun observeViewModel() {
+        lifecycleScope.launchWhenResumed {
+            viewModel.bfLocation.collect{
+                Log.d(REQUEST_TAG, "observe bfLocation: ${it}")
+                if (it != null) {
+                    putBfMarker()
+                }
+            }
+        }
+
+    }
+
     override fun onMapReady(nmap: NaverMap) {
         naverMap = nmap
+        observeViewModel()
         naverMap.apply {
             mapType = NaverMap.MapType.Basic
             setLayerGroupEnabled(NaverMap.LAYER_GROUP_TRANSIT, true)
@@ -54,20 +71,22 @@ class RequestMatchingFragment :
             )
             naverMap.moveCamera(CameraUpdate.toCameraPosition(cameraPosition))
             putGomdoriMarker()
-            putBfMarker()
         }
     }
 
     private fun putGomdoriMarker() {
-        Marker().apply {
+        if (viewModel.currentLocation != null) {
             val location = viewModel.currentLocation!!
-            position = LatLng(location.x, location.y)
-            icon = OverlayImage.fromResource(R.drawable.ic_marker_gomdori)
-            DensityUtil.setResouces(resources)
-            width = DensityUtil.dp2px(65f).toInt()
-            height = DensityUtil.dp2px(65f).toInt()
-            map = naverMap
+            Marker().apply {
+                position = LatLng(location.x, location.y)
+                icon = OverlayImage.fromResource(R.drawable.ic_marker_gomdori)
+                DensityUtil.setResouces(resources)
+                width = DensityUtil.dp2px(65f).toInt()
+                height = DensityUtil.dp2px(65f).toInt()
+                map = naverMap
+            }
         }
+
     }
 
     private fun putBfMarker(){
